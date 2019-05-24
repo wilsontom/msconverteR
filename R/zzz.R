fdaaf2a357145664a9dd0e2301458b8525ed766f
wilsontom <wilsontom89@gmail.com>
2019-05-24 14:48:12 +0100

    Fix rcmdcheck NOTE

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,9 @@
-.onLoad <-
-  function(libname = find.package('msconverteR'),
-           pkgname = 'msconverteR')
+.onAttach <-
+  function(...)
   {
     if (stevedore::docker_available() == FALSE) {
-      message(crayon::yellow('docker must be installed before using msconverteR'))
-      cat('\n')
-      message(crayon::yellow('visit https://docs.docker.com/install for more details'))
+      packageStartupMessage(crayon::yellow('docker must be installed before using msconverteR'), appendLF = TRUE)
+      packageStartupMessage(crayon::yellow('visit https://docs.docker.com/install for more details'))
     }
 
   }
