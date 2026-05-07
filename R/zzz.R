93535200c8156fb3a3754d6b4f7cfc86a4785388
Thomas Wilson [tpw2] (Staff) <tpw2@.aber.ac.uk>
2026-05-07 09:11:14 +0100

    Update R/zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <-
   function(...)
   {
-    if (stevedore::docker_available() == FALSE) {
+    if (.docker_available() == FALSE) {
       packageStartupMessage(crayon::yellow('docker must be installed before using msconverteR'), appendLF = TRUE)
       packageStartupMessage(crayon::yellow('visit https://docs.docker.com/install for more details'))
     }
